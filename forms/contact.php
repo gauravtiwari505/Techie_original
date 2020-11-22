c4c50ca2fa51efb97c83970bbfb5ac29ecd0f3e1
Gaurav Tiwari <gauravtiwari505@gmail.com>
2020-11-22 17:01:12 +0000

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,35 +7,102 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  // $receiving_email_address = 'gauravtiwari505@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  // // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  // /*
+  // $contact->smtp = array(
+  //   'host' => 'example.com',
+  //   'username' => 'example',
+  //   'password' => 'pass',
+  //   'port' => '587'
+  // );
+  // */
 
-  echo $contact->send();
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
+
+  // echo $contact->send();
+
+
+  if($_POST) {
+    $name = "";
+    $email = "";
+    $subject = "";
+    $message = "";
+    $email_body = "<div>";
+
+    if(isset($_POST['name'])) {
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Name:</b></label>&nbsp;<span>".$visitor_name."</span>
+                        </div>";
+    }
+
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Email:</b></label>&nbsp;<span>".$email."</span>
+                        </div>";
+    }
+
+    if(isset($_POST['subject'])) {
+        $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Subject:</b></label>&nbsp;<span>".$subject."</span>
+                        </div>";
+    }
+
+    // if(isset($_POST['concerned_department'])) {
+    //     $concerned_department = filter_var($_POST['concerned_department'], FILTER_SANITIZE_STRING);
+    //     $email_body .= "<div>
+    //                        <label><b>Concerned Department:</b></label>&nbsp;<span>".$concerned_department."</span>
+    //                     </div>";
+    // }
+
+    if(isset($_POST['message'])) {
+        $message = htmlspecialchars($_POST['message']);
+        $email_body .= "<div>
+                           <label><b>Message:</b></label>
+                           <div>".$message."</div>
+                        </div>";
+    }
+
+
+    $recipient = "gauravtiwari505@gmail.com";
+
+
+    $email_body .= "</div>";
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $email . "\r\n";
+
+    if(mail($recipient, $subject, $message, $headers)) {
+        echo "<p>Thank you for contacting us, $name. You will get a reply within 24 hours.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+
+} else {
+    echo '<p>Something went wrong</p>';
+}
+
+
 ?>
